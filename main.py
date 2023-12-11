b7c48db87ee5041250e25816570c700c67ae827b
Romy Rianata <wikiunlock0@gmail.com>
2023-12-12 01:44:25 +0800

    new file:   accounts.json 	modified:   main.py 	deleted:    accounts.json 	modified:   main.py

--- main.py
+++ main.py
@@ -1,159 +1,171 @@
 import tkinter as tk
-from tkinter import ttk, simpledialog, messagebox
+from tkinter import ttk, simpledialog, messagebox, filedialog
 import json
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet, InvalidToken
 import pyperclip
-from subprocess import Popen, CREATE_NEW_CONSOLE
+import secrets
 from ttkthemes import ThemedTk
+from zxcvbn import zxcvbn
 
 class SteamAccountManager:
     def __init__(self):
         self.root = ThemedTk(theme="equilux")
         self.root.title("Steam Account Manager")
 
-        self.accounts = self.load_accounts()
-        self.fernet = Fernet(Fernet.generate_key())
+        self.accounts = {}
+        self.real_license_key = self.load_real_license_key()
+        self.valid_license_keys = {self.real_license_key}
+        self.license_key = None
+        self.fernet = None
 
-        self.create_gui()
+        self.create_license_frame()
+
+    def load_real_license_key(self):
+        try:
+            with open("config.json", "r") as config_file:
+                config_data = json.load(config_file)
+                return config_data.get("real_license_key", "")
+        except FileNotFoundError:
+            return ""
 
     def load_accounts(self):
         try:
-            with open("accounts.json", "r") as file:
-                encrypted_data = json.load(file)
-                decrypted_data = {self.fernet.decrypt(key.encode()).decode(): value for key, value in encrypted_data.items()}
-                return decrypted_data
-        except (FileNotFoundError, json.JSONDecodeError):
+            with open("accounts.json", "rb") as file:
+                encrypted_data = file.read()
+                decrypted_data = self.fernet.decrypt(encrypted_data).decode()
+                return json.loads(decrypted_data)
+        except (FileNotFoundError, json.JSONDecodeError, InvalidToken):
             return {}
 
     def save_accounts(self):
-        encrypted_data = {self.fernet.encrypt(key.encode()).decode(): value for key, value in self.accounts.items()}
-        with open("accounts.json", "w") as file:
-            json.dump(encrypted_data, file, indent=4)
+        encrypted_data = self.fernet.encrypt(json.dumps(self.accounts).encode())
+        with open("accounts.json", "wb") as file:
+            file.write(encrypted_data)
 
-    def create_gui(self):
-        self.create_menu()
+    def create_license_frame(self):
+        license_frame = ttk.Frame(self.root)
 
-        style = ttk.Style()
-        style.configure("TButton", padding=6, relief="flat", background="#ccc")
-        style.map("TButton", foreground=[('pressed', 'black'), ('active', 'blue')])
+        ttk.Label(license_frame, text="License Key:").grid(row=0, column=0, padx=10, pady=10)
+        license_entry = ttk.Entry(license_frame, show='*')
+        license_entry.grid(row=0, column=1, padx=10, pady=10)
 
-        self.tree = ttk.Treeview(self.root, columns=("Username", "Password"), show="headings")
-        self.tree.heading("Username", text="Username")
-        self.tree.heading("Password", text="Password")
+        ttk.Button(license_frame, text="Unlock", command=lambda: self.unlock(license_entry.get())).grid(row=1, column=0, columnspan=2, pady=10)
 
-        self.tree.bind("<Double-1>", self.on_tree_double_click)
+        license_frame.grid(row=0, column=0, padx=50, pady=50)
 
-        self.tree.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+    def create_accounts_frame(self):
+        accounts_frame = ttk.Frame(self.root)
 
-        self.create_login_frame()
+        ttk.Button(accounts_frame, text="Add Account", command=self.add_account).grid(row=0, column=0, padx=10, pady=10)
+        ttk.Button(accounts_frame, text="Remove Account", command=self.remove_account).grid(row=0, column=1, padx=10, pady=10)
+        ttk.Button(accounts_frame, text="Launch Steam", command=self.launch_steam).grid(row=0, column=2, padx=10, pady=10)
+        ttk.Button(accounts_frame, text="Export Accounts", command=self.export_accounts).grid(row=0, column=3, padx=10, pady=10)
 
-        self.create_search_bar()
+        self.accounts_tree = ttk.Treeview(accounts_frame, columns=("Username", "Password", "Strength"), show="headings")
+        self.accounts_tree.heading("Username", text="Username")
+        self.accounts_tree.heading("Password", text="Password")
+        self.accounts_tree.heading("Strength", text="Strength")
 
-        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.accounts_tree.bind("<Double-1>", self.on_tree_double_click)
 
-    def create_menu(self):
-        menu_bar = tk.Menu(self.root)
-        self.root.config(menu=menu_bar)
+        self.accounts_tree.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
 
-        settings_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Settings", menu=settings_menu)
-        settings_menu.add_command(label="Add Account", command=self.add_account)
-        settings_menu.add_command(label="Remove Account", command=self.remove_account)
+        self.update_accounts_tree(self.accounts_tree)
 
-        launch_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Launch", menu=launch_menu)
-        launch_menu.add_command(label="Launch Steam", command=self.launch_steam)
+        accounts_frame.grid(row=1, column=0, padx=50, pady=50)
 
-    def create_login_frame(self):
-        for item in self.tree.get_children():
-            self.tree.delete(item)
+    def update_accounts_tree(self, tree):
+        for item in tree.get_children():
+            tree.delete(item)
 
         for account_name, account_info in self.accounts.items():
-            self.tree.insert("", "end", values=(account_name, "*" * len(account_info["password"])))
+            password_strength = zxcvbn(account_info["password"]).get("score", 0)
+            tree.insert("", "end", values=(account_name, "*" * len(account_info["password"]), password_strength))
 
-    def create_search_bar(self):
-        search_frame = ttk.Frame(self.root)
-        search_frame.grid(row=0, column=0, pady=10, sticky="nsew")
-
-        search_label = ttk.Label(search_frame, text="Search:")
-        search_label.grid(row=0, column=0, padx=5)
-
-        self.search_var = tk.StringVar()
-        search_entry = ttk.Entry(search_frame, textvariable=self.search_var)
-        search_entry.grid(row=0, column=1, padx=5)
-
-        search_button = ttk.Button(search_frame, text="Search", command=self.search_accounts)
-        search_button.grid(row=0, column=2, padx=5)
-
-        mask_button = ttk.Button(search_frame, text="Toggle Mask", command=self.toggle_mask)
-        mask_button.grid(row=0, column=3, padx=5)
-
-    def search_accounts(self):
-        search_term = self.search_var.get().lower()
-        if search_term:
-            matching_accounts = {name: info for name, info in self.accounts.items() if search_term in name.lower()}
-            self.create_login_frame(matching_accounts)
+    def unlock(self, entered_key):
+        if entered_key in self.valid_license_keys:
+            self.license_key = entered_key
+            self.fernet = Fernet(Fernet.generate_key())
+            self.accounts = self.load_accounts()
+            self.create_accounts_frame()
         else:
-            self.create_login_frame()
-
-    def toggle_mask(self):
-        for item in self.tree.selection():
-            account_name = self.tree.item(item, "values")[0]
-            account_info = self.accounts[account_name]
-            password_length = len(account_info["password"])
-            masked_password = "*" * password_length
-            self.tree.item(item, values=(account_name, masked_password))
+            messagebox.showerror("Invalid License Key", "The entered license key is invalid.")
 
     def add_account(self):
+        if not self.license_key:
+            messagebox.showinfo("Info", "Unlock the Account Manager first.")
+            return
+
         account_name = simpledialog.askstring("Add Account", "Enter account name")
         if account_name:
             username = simpledialog.askstring("Add Account", f"Enter username for {account_name}")
-            password = simpledialog.askstring("Add Account", f"Enter password for {account_name}", show='*')
+            generate_password = messagebox.askyesno("Generate Password", "Do you want to generate a strong password?")
+            if generate_password:
+                password = self.generate_strong_password()
+            else:
+                password = simpledialog.askstring("Add Account", f"Enter password for {account_name}", show='*')
 
             if username and password:
                 self.accounts[account_name] = {"username": username, "password": password}
                 self.save_accounts()
-                self.create_login_frame()
+                self.update_accounts_tree(self.accounts_tree)
 
     def remove_account(self):
-        selected_item = self.tree.selection()
+        if not self.license_key:
+            messagebox.showinfo("Info", "Unlock the Account Manager first.")
+            return
+
+        selected_item = self.accounts_tree.focus()
         if selected_item:
-            account_name = self.tree.item(selected_item, "values")[0]
+            account_name = self.accounts_tree.item(selected_item, "values")[0]
             confirmation = messagebox.askyesno("Confirmation", f"Are you sure you want to remove the account '{account_name}'?")
             if confirmation:
                 del self.accounts[account_name]
                 self.save_accounts()
-                self.create_login_frame()
+                self.update_accounts_tree(self.accounts_tree)
+
+    def launch_steam(self):
+        if not self.license_key:
+            messagebox.showinfo("Info", "Unlock the Account Manager first.")
+            return
+
+        selected_item = self.accounts_tree.focus()
+        if selected_item:
+            account_name = self.accounts_tree.item(selected_item, "values")[0]
+            self.login_steam(account_name)
+        else:
+            messagebox.showinfo("Info", "Select an account to launch.")
 
     def on_tree_double_click(self, event):
-        selected_item = self.tree.selection()
+        if not self.license_key:
+            messagebox.showinfo("Info", "Unlock the Account Manager first.")
+            return
+
+        selected_item = self.accounts_tree.focus()
         if selected_item:
-            account_name = self.tree.item(selected_item, "values")[0]
+            account_name = self.accounts_tree.item(selected_item, "values")[0]
             username = self.accounts[account_name]["username"]
             password = self.accounts[account_name]["password"]
 
             pyperclip.copy(f"Username: {username}\nPassword: {password}")
             messagebox.showinfo("Copy to Clipboard", "Username and Password copied to clipboard.")
 
-    def login(self, account_name):
-        account = self.accounts[account_name]
-        command = f'start steam.exe -login {account["username"]} {account["password"]}'
-        try:
-            Popen(command, shell=True, creationflags=CREATE_NEW_CONSOLE)
-        except Exception as e:
-            messagebox.showerror("Error", f"Error launching Steam: {e}")
+    def generate_strong_password(self):
+        password = secrets.token_urlsafe(16)
+        messagebox.showinfo("Generated Password", f"Generated Password: {password}")
+        return password
 
-    def launch_steam(self):
-        selected_item = self.tree.selection()
-        if selected_item:
-            account_name = self.tree.item(selected_item, "values")[0]
-            self.login(account_name)
-        else:
-            messagebox.showinfo("Info", "Select an account to launch.")
+    def export_accounts(self):
+        if not self.license_key:
+            messagebox.showinfo("Info", "Unlock the Account Manager first.")
+            return
 
-    def on_close(self):
-        self.save_accounts()
-        self.root.destroy()
+        file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json")])
+        if file_path:
+            with open(file_path, "wb") as file:
+                encrypted_data = self.fernet.encrypt(json.dumps(self.accounts).encode())
+                file.write(encrypted_data)
+            messagebox.showinfo("Export Successful", "Accounts exported successfully.")
 
     def run(self):
         self.root.mainloop()
